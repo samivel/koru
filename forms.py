74e46c1a074f344934a74c307d5b2a54bf4e596e
Samivel Evans <samivel@gmail.com>
2020-08-09 18:11:05 -0400

    Database sqlalchemy configured

--- forms.py
+++ forms.py
@@ -12,7 +12,7 @@ class RegistrationForm(FlaskForm):
 
     email = StringField('Email', validators=[DataRequired(), Email()])
 
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 
     submit = SubmitField('Register')
